1e2a36c3fe02e4fc2fe85883d4dd1af9ebe2ad36
ismael strey <ismaelstrey@hotmail.com>
2019-03-13 09:48:31 -0300

    remove resto

--- database/migrations/2019_03_11_140404_add_imagem_teble_users.php
+++ database/migrations/2019_03_11_140404_add_imagem_teble_users.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateImageUser extends Migration
+class AddImagemTebleUsers extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class CreateImageUser extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('image')->nullable();
+            $table->string('imagem')->nullable();
         });
     }
 
@@ -26,7 +26,7 @@ class CreateImageUser extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('image');
+            $table->dropColumn('imagem');
         });
     }
 }
